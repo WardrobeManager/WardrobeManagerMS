d7240fc148aa27a97c18e34843b29f4c51c26087
Prashant Desai <prashant_desai@yahoo.com>
2021-08-15 21:55:17 -0700

    updated create outfit

--- test/createOutfitJson.sh
+++ test/createOutfitJson.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "{\"top-id\":\"goo\",\"bottom-id\":\"goo\",\"description\":\"new outfit\"}" > $1
+echo "{\"top-id\":\"6c555e40-24fb-42a3-afeb-f7dc0763a88f\",\"bottom-id\":\"goo\",\"description\":\"new outfit\"}" > $1
